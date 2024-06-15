b433c3db501b748831090ae16a6c046ba4ebd92c
Héric Gomes da Silva <h1g2s0@hotmail.com>
2024-06-14 21:06:13 -0300

    js

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+const btnPlataforma = document.querySelector('.btn-plataforma');
+const elementoPlataformas = document.querySelector('.btn-plataforma .plataformas');
+
+btnPlataforma.addEventListener('click', () => {
+    elementoPlataformas.classList.toggle('ativo');
+});
